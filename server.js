cb0482f0d2421b5355958bbef83e6437304ae3f3
DavideDeFeudis <davidedefeudis@gmail.com>
2020-03-15 23:17:39 +0100

    Express validator

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const { check, body, validationResult } = require("express-validator")
 const sendMail = require('./mail');
 const mongoose = require('mongoose');
 require('dotenv').config();
@@ -13,11 +14,8 @@ mongoose.connect(mongoUrl, {
     useFindAndModify: false,
     useUnifiedTopology: true
 }, (err) => {
-    if (!err) {
-        console.log('MongoDB Connection succeeded')
-    } else {
-        console.log('Error on DB connection: ' + err)
-    }
+    if (!err) console.log('MongoDB Connection succeeded')
+    else console.log('Error on DB connection: ' + err)
 });
 
 const UserSchema = new mongoose.Schema({
@@ -41,7 +39,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/version', (req, res) => {
-    res.json({ message: 'form version 1' })
+    res.json({ message: 'Form version 1' })
 })
 
 app.get('/users', async (req, res) => {
@@ -50,11 +48,34 @@ app.get('/users', async (req, res) => {
         res.send(users)
     } catch (err) {
         console.log(err)
-        res.status(500).json({ message: 'Error getting users', err })
+        res.status(500).json({ message: 'Error getting users.', err })
     }
 })
 
-app.post('/contact', async (req, res) => {
+const validation = [
+    body('name').trim().isLength({ min: 2 }).withMessage('Name is too short.'),
+    body('email', 'Incorrect email format.').normalizeEmail().trim().isEmail(),
+    body('beschreibungstext').trim().escape()
+]
+
+const createUser = async (req, res) => {
+    const user = new User(req.body)
+    try {
+        await user.save()
+        console.log('User created.')
+        res.json({ success: true, message: 'User created.' })
+    } catch (err) {
+        console.log('Error creating user.', err)
+        res.status(500).json({ success: false, message: 'Error creating user.', err })
+    }
+}
+
+app.post('/contact', validation, (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
     let { name, email, anfrage, beschreibungstext } = req.body
     console.log('req.body:', req.body)
     const subject = anfrage
@@ -63,7 +84,6 @@ app.post('/contact', async (req, res) => {
     } else {
         text = anfrage
     }
-
     sendMail(email, name, subject, text, (err, data) => {
         if (err) {
             console.log('Error sending email.', err)
@@ -71,29 +91,20 @@ app.post('/contact', async (req, res) => {
         }
         else {
             console.log('Your email has been sent.')
-            res.json({ success: true, message: 'Your email has been sent.' })
+            createUser(req, res)
+            // res.json({ success: true, message: 'Your email has been sent.' })
         }
     })
-
-    const user = new User(req.body)
-    try {
-        await user.save()
-        res.json({ success: true, message: 'User created' })
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({ success: false, message: 'Error creating user', err })
-    }
 })
 
 app.get('/drop', (req, res) => {
     User.collection.drop()
-        .then(() => res.send({ message: 'User collection dropped' }))
+        .then(() => res.send({ message: 'User collection dropped.' }))
         .catch(err => res.send(err))
 })
 
-// // ERROR HANDLER
-// app.use((err, req, res, next) => {
-//     res.status(500).send({ error: err.message || err })
-// })
+app.use((err, req, res, next) => {
+    res.status(500).send({ error: err.message || err }) // error handler
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
